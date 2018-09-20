b1140d39b317fa5f885f4dac95f5dc61e99952d7
Ben Konrath <ben@bagu.org>
2018-09-20 09:06:41 +0200

    Add testing for Django 2.0 and 2.1.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,3 @@
-import django
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -10,8 +8,12 @@ DATABASES = {
 SECRET_KEY = 'fake-key'
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+
     'csv_export',
     'tests',
 ]
@@ -19,8 +21,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
 USE_TZ = True
 
 ROOT_URLCONF = 'tests.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        }
+    }
+]
