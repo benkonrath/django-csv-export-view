9504564716f7f0224fd486376cddacb5ea38f501
Ben Konrath <ben@bagu.org>
2021-05-17 10:35:43 +0200

    Fix PyPy test failures on Travis CI (#13)

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -44,3 +44,17 @@ TEMPLATES = [
 
 SILENCED_SYSTEM_CHECKS = ('models.W042',)
 
+
+# Disable migrations in tests because they don't work with pypy on Travis with PyPy
+# 3.7.1. The tests work locally on PyPy 3.7.4.
+# http://stackoverflow.com/questions/25161425/disable-migrations-when-running-unit-tests-in-django-1-7
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
+
